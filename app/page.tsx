f297aa14958da606ff7fefd139fd8c4a6b140aaf
Adam Smith <adam@roadlyfe.com>
2023-02-07 14:50:29 -0800

    implementing initial UI

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,53 @@
-import React from "react";
+import { BoltIcon, ExclamationTriangleIcon, SunIcon } from '@heroicons/react/24/outline'
+
 
 function HomePage() {
     return (
-        <div>HomePage</div>
-    )
-}
+        <div className="flex flex-col items-center justify-center h-screen px-2 text-white">
+            <h1 className="text-5xl font-bold mb-20">ChatGPT</h1>
+            <div className='flex space-x-2 text-center'>
+                <div>
+                    <div className="flex flex-col items-center justify-centermb-5">
+                        {/* Sun Icon */}
+                        <SunIcon className="h-8 w-8" />
+                        <h2>Examples</h2>
+                    </div>
 
-export default HomePage
+                    <div className="space-y-2">
+                        <p className="infoText">"Explain something to me"</p>
+                        <p className="infoText">"What is the differnce between a dog and a cat?"</p>
+                        <p className="infoText">"What is the color of the sun?"</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="flex flex-col items-center justify-centermb-5">
+                        {/* Sun Icon */}
+                        <BoltIcon className="h-8 w-8" />
+                        <h2>Capabilities</h2>
+                    </div>
+
+                    <div className="space-y-2">
+                        <p className="infoText">"Change the ChatGPT model to use"</p>
+                        <p className="infoText">"Messages are stored in FireBase's Firestore"</p>
+                        <p className="infoText">"Hot Toast notificatons when ChatGpt is thinking!"</p>
+                    </div>
+                </div>
+                <div>
+                    <div className="flex flex-col items-center justify-centermb-5">
+                        {/* Sun Icon */}
+                        <ExclamationTriangleIcon className="h-8 w-8" />
+                        <h2>Limitations</h2>
+                    </div>
+
+                    <div className="space-y-2">
+                        <p className="infoText">"May occasionally generate incorrect information"</p>
+                        <p className="infoText">"May occasionally produce harmful instructions or biased content"</p>
+                        <p className="infoText">"Limited knowledge of world and events after 2021"</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+};
+
+export default HomePage;
