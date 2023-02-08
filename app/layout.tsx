c83b5f9d489a01a62e650fc54078ed52811f83e9
Adam Smith <adam@roadlyfe.com>
2023-02-07 17:08:52 -0800

    login auth implemented

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,35 @@
 import SideBar from '../components/SideBar';
 import '../styles/globals.css';
+import { getServerSession } from 'next-auth';
+import { SessionProvider } from '../components/SessionProvider';
+import { authOptions } from '../pages/api/auth/[...nextauth]';
+import Login from '../components/Login';
 
-export default function RootLayout({
+export default async function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const session = await getServerSession(authOptions)
+  // console.log(session)
   return (
     <html>
       <head />
       <body>
-        <div className='flex'> 
+        <SessionProvider session={session}>
+          {!session ? (
+            <Login />
+          ): (
+
+            <div className='flex'> 
           <div className='bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]'>
             <SideBar />
           </div>
           {/* clientProvider - notifications */}
         <div className='bg-[#343541] flex-1'>{children}</div>
         </div>
+          )}
+        </SessionProvider>
         </body>
     </html>
   )
