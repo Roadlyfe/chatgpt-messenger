ef26f21b5ef50a1dcfb0cfe8fab6cc9a175be3c8
Adam Smith <adam@roadlyfe.com>
2023-02-09 14:57:23 -0800

    connected the openai api to the app

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,13 +1,17 @@
+'use client';
+
+import { useSession } from "next-auth/react";
+
 type Props = {
   chatId: string;
 };
 
 
 function Chat({ chatId }: Props ) {
+  const { data: session } = useSession();
+
   return (
-    <div className="flex-1">
-      Chat
-    </div>
+    <div className="flex-1">Chat</div>
   )
 }
 
