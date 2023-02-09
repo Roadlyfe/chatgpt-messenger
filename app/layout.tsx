e100d888c504912cb2297e689723fb3a880b5672
Adam Smith <adam@roadlyfe.com>
2023-02-09 10:54:14 -0800

    created and implemented the typings page and clientProvder page

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import { getServerSession } from 'next-auth';
 import { SessionProvider } from '../components/SessionProvider';
 import { authOptions } from '../pages/api/auth/[...nextauth]';
 import Login from '../components/Login';
+import ClientProvider from '../components/ClientProvider';
 
 export default async function RootLayout({
   children,
@@ -26,6 +27,7 @@ export default async function RootLayout({
             <SideBar />
           </div>
           {/* clientProvider - notifications */}
+          <ClientProvider />
         <div className='bg-[#343541] flex-1'>{children}</div>
         </div>
           )}
